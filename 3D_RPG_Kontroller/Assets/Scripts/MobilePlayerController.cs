bcda1041b8d3eb1a03f8b7dee7618ca02896caff
Artsem <gorbatchevartsem@gmail.com>
2019-10-14 16:12:33 +0300

    First version of 3d rpg controller

---
+++ 3D_RPG_Kontroller/Assets/Scripts/MobilePlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class MobilePlayerController : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    private Image joystickBG;
+    [SerializeField]
+    private Image joystick;
+    private Vector2 inputVector;
+
+    private void Start()
+    {
+        joystickBG = GetComponent<Image>();
+        joystick = transform.GetChild(0).GetComponent<Image>();
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 pos;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(joystickBG.rectTransform, eventData.position, eventData.pressEventCamera, out pos))
+        {
+            pos.x = (pos.x / joystickBG.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / joystickBG.rectTransform.sizeDelta.x);
+
+            inputVector = new Vector2(pos.x * 2 - 1, pos.y * 2 - 1);
+            inputVector = (inputVector.magnitude > 1.0f) ? inputVector.normalized : inputVector;
+
+            Debug.Log(inputVector);
+
+            joystick.rectTransform.anchoredPosition = new Vector2(inputVector.x * (joystickBG.rectTransform.sizeDelta.x / 2), inputVector.y * (joystickBG.rectTransform.sizeDelta.y / 2));
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        OnDrag(eventData);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        inputVector = Vector2.zero;
+        joystick.rectTransform.anchoredPosition = Vector2.zero;
+    }
+
+    public float Horizontal()
+    {
+        return inputVector.x;
+    }
+
+    public float Vertical()
+    {
+        return inputVector.y;
+    }
+}
---
+++ 3D_RPG_Kontroller/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //main parameters
+    public float speedMove;
+    public float jumpPower;
+    public float hitRate;
+    public float nextHit = -1f;
+
+    //gameplay parameters
+    private float gravityForce;
+    private Vector3 moveVector; //character movement direction
+
+    //component links
+    private CharacterController ch_controller;
+    private Animator ch_animator;
+    private MobilePlayerController mob_ch_contr;
+
+    void Start()
+    {
+        ch_controller = GetComponent<CharacterController>();
+        ch_animator = GetComponent<Animator>();
+        mob_ch_contr = GameObject.FindGameObjectWithTag("Joystick").GetComponent<MobilePlayerController>();
+    }
+
+    void Update()
+    {
+        CharacterMove();
+        GamingGravity();
+    }
+
+    private void CharacterMove()
+    {
+        if (ch_controller.isGrounded)
+        {
+            //set character movement direction
+            moveVector = Vector3.zero;
+            moveVector.x = mob_ch_contr.Horizontal() * speedMove; //Input.GetAxis("Horizontal")
+            moveVector.z = mob_ch_contr.Vertical() * speedMove; //Input.GetAxis("Vertical")
+
+            //анимация передвижения персонажа при его перемещении
+            if (moveVector.x != 0 || moveVector.z != 0)
+            {
+                ch_animator.SetBool("Move", true);
+                float playerSpeed = Mathf.Sqrt(Mathf.Pow(mob_ch_contr.Horizontal(), 2) + Mathf.Pow(mob_ch_contr.Vertical(), 2));
+                ch_animator.SetFloat("Speed", playerSpeed);
+            }
+            else
+                ch_animator.SetBool("Move", false);
+
+            //rotation of characters in the direction of movement
+            if (Vector3.Angle(Vector3.forward, moveVector) > 1f || Vector3.Angle(Vector3.forward, moveVector) == 0)
+            {
+                Vector3 direction = Vector3.RotateTowards(transform.forward, moveVector, speedMove, 0.0f);
+                transform.rotation = Quaternion.LookRotation(direction);
+            }
+        }
+
+        moveVector.y = gravityForce;
+        ch_controller.Move(moveVector * Time.deltaTime);
+    }
+
+    private void GamingGravity()
+    {
+        if (!ch_controller.isGrounded)
+            gravityForce -= 20f * Time.deltaTime;
+        else
+            gravityForce = -1f;
+
+        //CharacterJump()
+    }
+
+    public void CharacterJump()
+    {
+        if (ch_controller.isGrounded) // && Input.GetKeyDown(KeyCode.Space)
+        {
+            gravityForce = jumpPower;
+        }
+    }
+
+    public void CharacterHit()
+    {
+        if(Time.time > nextHit && ch_controller.isGrounded)
+        {
+            nextHit = Time.time + hitRate;
+            Debug.Log("Hit");
+
+            //add fit animation with trigger "Hit"
+        }
+    }
+}
